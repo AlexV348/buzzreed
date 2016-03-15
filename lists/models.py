c51ddfa8e40a367ef6c8040f61501d4fc2ad1ad2
alex <AlexV348@gmail.com>
2016-03-14 19:44:50 -0700

    added a list model

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class List(models.Model):
+    '''
+    A list of a bunch of items that will be displayed
+    ie 28 things to remember when making fondue or something
+    '''
+    title = models.CharField(max_length=200)
+    creator = models.ForeignKey(
+        User,
+        models.SET_NULL,
+        blank=True, null=True,
+    )
